9a919be33193b43e82c9c5703ab82a27eca22199
Varnikachaudhary1234 <varnikachaudhary2004@gmail.com>
2026-05-28 15:19:45 +0530

    backend

---
+++ backend/apiCall.js
@@ -0,0 +1,15 @@
+// function sum(num1,num2){
+// return num1+num2;
+
+// }
+
+
+async function getServerData(){
+    const serverData= await fetch('https://fakestoreapi.com/products')
+    const jsondata=await serverData.json();
+    console.log(jsondata)
+    return jsondata;
+
+}
+
+module.exports=getServerData;
